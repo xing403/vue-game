da18c33ff31b9066ca6747e5880d51a82435aa64
星如雨 <liyun200104@163.com>
2023-06-20 15:41:13 +0800

    增加等级模式

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,14 +4,36 @@ import type { BlockState } from '~/type'
 // 更换页面标题
 useTitle('MinesWeeper')
 
-const WIDTH = 5
-const HEIGHT = 5
-
 let generate = false
-const mineNum = ref(0)
+const mineNum = ref(15)
 const flagNum = ref(0)
 const gameState = ref<'play' | 'won' | 'lost'>('play')
+const gameLevel = ref<'simple' | 'general' | 'difficult' | 'hell'>('general')
+const gameLevels = ['simple', 'general', 'difficult', 'hell']
+const gameConfig = {
+  simple: {
+    mineNum: 10,
+    width: 5,
+    height: 5,
+  },
+  general: {
+    mineNum: 40,
+    width: 15,
+    height: 15,
+  },
+  difficult: {
+    mineNum: 60,
+    width: 15,
+    height: 15,
+  },
+  hell: {
+    mineNum: 80,
+    width: 15,
+    height: 15,
+  },
 
+}
+const gameSettingDialog = ref(false)
 const directions = [
   [-1, 0],
   [-1, -1],
@@ -27,8 +49,10 @@ const state = ref<BlockState[][]>([])
 function reset() {
   gameState.value = 'play'
   generate = false
-  state.value = Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from({ length: WIDTH },
+  flagNum.value = 0
+  mineNum.value = gameConfig[gameLevel.value].mineNum
+  state.value = Array.from({ length: gameConfig[gameLevel.value].height }, (_, y) =>
+    Array.from({ length: gameConfig[gameLevel.value].width },
       (_, x): BlockState => ({
         x, y, adjacentMines: 0,
       }),
@@ -49,23 +73,26 @@ function onRightClick(block: BlockState) {
   }
   checkGameState()
 }
-function generateMines(state: BlockState[][], initial: BlockState) {
-  mineNum.value = 0
+function generateMines(state: BlockState[][], initial: BlockState, length: number) {
   flagNum.value = 0
-  for (const row of state) {
-    for (const block of row) {
-      if (Math.abs(initial.x - block.x) <= 1 && Math.abs(initial.y - block.y) <= 1)
-        continue
-      if (Math.random() < 0.2) {
-        mineNum.value += 1
-        block.mine = true
-      }
-    }
-  }
-  if (!mineNum.value)
-    generateMines(state, initial)
-
+  generateMine(state, initial, length)
   updateNumbers(state)
+}
+function generateMine(state: BlockState[][], initial: BlockState, length: number) {
+  if (length === 0)
+    return
+  const x = randomInt(0, gameConfig[gameLevel.value].width)
+  const y = randomInt(0, gameConfig[gameLevel.value].height)
+  if (Math.abs(initial.x - x) <= 1 && Math.abs(initial.y - y) <= 1) {
+    generateMine(state, initial, length)
+  }
+  else if (!state[y][x].mine) {
+    state[y][x].mine = true
+    generateMine(state, initial, length - 1)
+  }
+  else {
+    generateMine(state, initial, length)
+  }
 }
 function updateNumbers(state: BlockState[][]) {
   state.forEach((row, y) => {
@@ -84,7 +111,7 @@ function onClick(block: BlockState) {
     return
   block.revealed = true
   if (!generate) {
-    generateMines(state.value, block)
+    generateMines(state.value, block, mineNum.value)
     generate = true
   }
   if (block.mine) {
@@ -109,7 +136,7 @@ function getSiblings(block: BlockState) {
   return directions.map(([ox, oy]) => {
     const dx = block.x + ox
     const dy = block.y + oy
-    if (dx < 0 || dy < 0 || dx >= WIDTH || dy >= HEIGHT)
+    if (dx < 0 || dy < 0 || dx >= gameConfig[gameLevel.value].width || dy >= gameConfig[gameLevel.value].height)
       return undefined
     return state.value[dy][dx]
   }).filter(Boolean) as BlockState[]
@@ -130,7 +157,22 @@ function showAllMines() {
       i.revealed = true
   })
 }
+function handleGameSetting() {
+  gameSettingDialog.value = true
+}
+function handleChangeGameSetting() {
+  gameSettingDialog.value = false
+  reset()
+}
 watchEffect(checkGameState)
+function random(min: number, max: number) {
+  return Math.random() * (max - min) + min
+}
+
+function randomInt(min: number, max: number) {
+  return Math.floor(random(min, max))
+}
+
 reset()
 </script>
 
@@ -163,6 +205,23 @@ reset()
 
     <nav mt-6 inline-flex gap-2 text-xl>
       <button icon-btn i-carbon-sun dark:i-carbon-moon @click="toggleDark()" />
+      <button icon-btn i-carbon-settings @click="handleGameSetting()" />
     </nav>
+    <el-dialog v-model="gameSettingDialog" title="游戏设置">
+      <el-form label-position="top" label-width="100px" style="max-width: 460px">
+        <el-form-item label="游戏等级">
+          <el-radio-group v-model="gameLevel">
+            <el-radio v-for="item, index in gameLevels" :key="index" :label="item" @change="handleChangeGameSetting">
+              {{ item }}
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="gameSettingDialog = false">
+          取消
+        </el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
